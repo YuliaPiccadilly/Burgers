35e95c830c8f30e3ee437cedcb6f049d7f7d230f
Julia Pletneva <juliarf@inbox.ru>
2023-11-09 16:19:08 +0300

    api

--- js/const.js
+++ js/const.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:3024';
+//export const API_URL = 'http://localhost:3024';
+export const API_URL = 'https://foggy-curved-mule.glitch.me';
 export const PREFIX_PRODUCT ='/api/product';
